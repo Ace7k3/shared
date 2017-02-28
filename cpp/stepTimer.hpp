02c58d4b7384ff51d4ebfbef1d5725f2d2a6d79f
Benjamin Cabrera <benjamin.cabrera@uni-due.de>
2017-02-28 10:08:19 +0100

    Switched the step timer to header-only

---
+++ cpp/stepTimer.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <map>
+#include <chrono>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+
+class StepTimer {
+
+	public:
+
+		inline void start_timing_step(const std::string label, const std::string description)
+		{
+			if(_timers.find(label) != _timers.end())
+				throw std::logic_error("Tried to start timer with existing label again");
+
+			_timers.insert({ label, { description, std::chrono::steady_clock::now() } });
+		}
+
+		inline void stop_timing_step(const std::string label)
+		{
+			auto it = _timers.find(label);
+			if(it == _timers.end())
+				throw std::logic_error("Tried to stop non-started timer.");
+
+			auto endTime = std::chrono::steady_clock::now();
+			auto diff = std::chrono::duration<double, std::milli>(endTime-it->second.second).count();
+			_finished_timings.push_back({ it->second.first, diff });
+
+			_timers.erase(it);
+		}
+
+		inline void print_timings(std::ostream& ostr) const
+		{
+			std::size_t maxStrLength = 0;
+			for(auto timing : _finished_timings)
+				maxStrLength = std::max(maxStrLength, timing.first.length());
+
+			for(auto timing : _finished_timings)
+				ostr << std::left << std::setw(maxStrLength+3) << timing.first + ": " << timing_to_readable(timing.second) << std::endl;
+		}
+
+
+	private:
+		std::vector<std::pair<std::string, double>> _finished_timings;
+		std::map<std::string, std::pair<std::string, std::chrono::time_point<std::chrono::steady_clock>>> _timers;
+
+		inline std::string timing_to_readable(std::size_t milliseconds) const
+		{
+			std::stringstream ss;
+
+			auto hours = (milliseconds / (1000*60*60));
+			auto mins =  (milliseconds % (1000*60*60)) / (1000*60);
+			auto seconds =  ((milliseconds % (1000*60*60)) % (1000*60)) / 1000;
+			auto milli =  (((milliseconds % (1000*60*60)) % (1000*60)) % 1000);
+			ss << hours << "h " << std::setw(2) << mins << "m " << std::setw(2) << seconds << "s " << std::setw(4) << milli << "ms";
+
+			return ss.str();
+		}
+};
